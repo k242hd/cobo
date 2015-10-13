1be4b84bdee30fde120aa71d44aa6b1f8f36c5f2
t_murano <viceversa.ue700@gmail.com>
2015-10-13 22:17:51 +0900

    add header row to view

--- view.go
+++ view.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/nsf/termbox-go"
+import (
+	"github.com/mattn/go-runewidth"
+	"github.com/nsf/termbox-go"
+	"strings"
+)
 
 type View struct {
 	height int
@@ -18,11 +22,12 @@ func InitView(m *Model) *View {
 		quit:   false,
 	}
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	v.printHeader()
 	for y, c := range m.contents {
 		if y == 0 {
-			v.println(y, c, true)
+			v.println(y+1, c, true)
 		} else {
-			v.println(y, c, false)
+			v.println(y+1, c, false)
 		}
 	}
 	return v
@@ -33,21 +38,37 @@ func (v *View) Exit() {
 }
 
 func (v *View) LineUp(m *Model) {
-	if v.ptr == 0 {
+	if v.ptr == 1 {
 		return
 	}
-	v.println(v.ptr, m.contents[v.ptr], false)
+	v.println(v.ptr, m.contents[v.ptr-1], false)
 	v.ptr--
-	v.println(v.ptr, m.contents[v.ptr], true)
+	v.println(v.ptr, m.contents[v.ptr-1], true)
 }
 
 func (v *View) LineDown(m *Model) {
-	if v.ptr == len(m.contents)-1 {
+	if v.ptr == len(m.contents) {
 		return
 	}
-	v.println(v.ptr, m.contents[v.ptr], false)
+	v.println(v.ptr, m.contents[v.ptr-1], false)
 	v.ptr++
-	v.println(v.ptr, m.contents[v.ptr], true)
+	v.println(v.ptr, m.contents[v.ptr-1], true)
+}
+
+func (v *View) printHeader() {
+	hedderLeft := "Command"
+	hedderRight := "Memo"
+	v.printTwoColumn(0, hedderLeft, hedderRight, false)
+}
+
+func (v *View) printTwoColumn(y int, left, right string, highlight bool) {
+	leftLimit := v.width / 2
+	rightLimit := (v.width - 1) / 2
+	leftTruncated := runewidth.Truncate(left, leftLimit, "")
+	rightTruncated := runewidth.Truncate(right, rightLimit, "")
+	leftFilled := leftTruncated + strings.Repeat(" ", leftLimit-len(leftTruncated))
+	rowMsg := leftFilled + "|" + rightTruncated
+	v.println(y, rowMsg, highlight)
 }
 
 func (v *View) println(y int, msg string, highlight bool) {
